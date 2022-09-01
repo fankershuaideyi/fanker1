40a483a3f9a5279a6499ecee08367cdd2de968a7
fanker <205226@stu.hebut.edu.cn>
2022-09-01 15:49:35 +0800

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -20,6 +20,9 @@ public class Greeter {
     String language = args[0];
     String country = args[1];
     String name = args[2];
+    if(name.length()>5){
+      System.out.println("name > 5")
+    }
     Greeter greeter = new Greeter(language, country, name);
     System.out.println(greeter.sayHello());
   }
